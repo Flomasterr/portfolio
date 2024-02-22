5072e12bbc8320876a27331586b3330393041c16
Flomasterr <32779728+Flomasterr@users.noreply.github.com>
2024-02-22 17:40:56 +0200

    Add files via upload

--- monito-pets-store/js/app.js
+++ monito-pets-store/js/app.js
@@ -4852,7 +4852,7 @@
         };
         const da = new DynamicAdapt("max");
         da.init();
-        window["FLS"] = true;
+        window["FLS"] = false;
         isWebp();
         menuInit();
         showMore();
